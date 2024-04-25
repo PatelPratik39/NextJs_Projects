9990009223de0218db721fc76d54598d018e1b80
PatelPratik39 <ptl.pratik717@gmail.com>
2024-04-25 12:16:31 -0500

    chapter 7 -  routing deom different components

--- nextjs-dashboard/app/lib/placeholder-data.js
+++ nextjs-dashboard/app/lib/placeholder-data.js
@@ -12,20 +12,20 @@ const users = [
 const customers = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
+    name: 'Lalloo Lal',
+    email: 'lalloo@lamba.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
+    name: 'Mangoo Mochi',
+    email: 'mangoo@mochi.com',
     image_url: '/customers/lee-robinson.png',
   },
   {
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
-    name: 'Hector Simpson',
-    email: 'hector@simpson.com',
+    name: 'Kapser Chandu',
+    email: 'kasperlal@chandu.com',
     image_url: '/customers/hector-simpson.png',
   },
   {
